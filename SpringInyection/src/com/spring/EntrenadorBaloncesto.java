dcd81811858e32b2f050a99defba1ccaf4116471
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2023-03-10 13:55:39 +0100

    Inyeccion por SETTERS

--- SpringInyection/src/com/spring/EntrenadorBaloncesto.java
+++ SpringInyection/src/com/spring/EntrenadorBaloncesto.java
@@ -2,18 +2,19 @@ package com.spring;
 
 public class EntrenadorBaloncesto implements Entrenador {
 
+	private ExperienciaServicio experienciaServicio; 
 	@Override
-	public String getEntrenamiento() {
-		// TODO Auto-generated method stub
-		return null;
+	public String getEntrenamiento() { 
+		return "Lanzar 30 tiros a canasta";
 	}
-
 	@Override
 	public int getExperiencia() {
-		// TODO Auto-generated method stub
-		return 0;
+		return experienciaServicio.getExperiencia();
 	}
-
-	
+	// inyección del servicio en un método setter
+	public void setExperienciaServicio(ExperienciaServicio experienciaServicio) { 
+		System.out.println("Inyección en un método setter"); 
+		this.experienciaServicio = experienciaServicio;
+	}
 	
 }
--- SpringInyection/src/com/spring/MainSpring.java
+++ SpringInyection/src/com/spring/MainSpring.java
@@ -9,9 +9,14 @@ public class MainSpring {
 		ClassPathXmlApplicationContext context =
 		new ClassPathXmlApplicationContext("applicationContext.xml");
 		
-		// obtener el bean
+		// obtener el bean por constructor
 		Entrenador entrenador = context.getBean("mientrenador", Entrenador.class); 
 		System.out.println(entrenador.getExperiencia());
+		
+		// obtener el bean por setter
+		Entrenador entrenador2 = context.getBean("mientrenadorBaloncesto"
+				, Entrenador.class); 
+		System.out.println(entrenador2.getExperiencia());
 		
 		// cierra el context
 		context.close();
